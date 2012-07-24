de48e4696ed7fab11003c1eee8a04755a5ecf6a1
Ha Hong <hahong84@gmail.com>
2012-07-24 11:36:40 -0400

    MISC: small errors and cosmetic changes

--- bangmetric/dprime.py
+++ bangmetric/dprime.py
@@ -10,35 +10,53 @@ __all__ = ['dprime', 'dprime_from_confusion_matrix']
 
 import numpy as np
 from scipy.stats import norm
-from .utils import confusion_stats
+from .utils import confusion_matrix_stats
+
+DEFAULT_DPRIME_MODE = 'binary'
 
 
-def dprime(y_pred=None, y_true=None, pos=None, neg=None, max_value=np.inf, min_value=-np.inf):
-    """Computes the d-prime sensitivity index.
-    One must provide either y_pred and y_true or pos and neg.
-    This function computes the d-prime of predictions given by 
-    y_pred and y_true by default. If pos and neg are provided,
-    both y_pred and y_true are ignored, and this function 
-    computes the d-prime from positive and negative samples
-    given by pos and neg.
+def dprime(A, B=None, mode=DEFAULT_DPRIME_MODE, max_value=np.inf,\
+        min_value=-np.inf):
+    """Computes the d-prime sensitivity index of predictions
+    from various data formats.  Depending on the choice of
+    `mode`, this function can take one of the following format:
+
+    * Binary classification outputs (`mode='binary'`; default)
+    * Positive and negative samples (`mode='sample'`)
+    * True positive and false positive rate (`mode='rate'`)
 
     Parameters
     ----------
-    y_true: array, shape = [n_samples], optional
-        True values, interpreted as strictly positive or not
-        (i.e. converted to binary).
-        Could be in {-1, +1} or {0, 1} or {False, True}.
+    A, B:
+        If `mode` is 'binary':
 
-    y_pred: array, shape = [n_samples], optional
-        Predicted values (real).
+            A: array, shape = [n_samples],
+                True values, interpreted as strictly positive or not
+                (i.e. converted to binary).
+                Could be in {-1, +1} or {0, 1} or {False, True}.
 
-    pos: array-like, optional
-        Positive sample values (e.g., raw projection values of 
-        the positive classifier).
+            B: array, shape = [n_samples],
+                Predicted values (real).
 
-    neg: array-like, optional
-        Negative sample values. If both pos and neg are
-        provided, y_true and y_pred are ignored.
+        If `mode` is 'sample':
+
+            A: array-like,
+                Positive sample values (e.g., raw projection values
+                of the positive classifier).
+
+            B: array-like,
+                Negative sample values.
+
+        If `mode` is 'rate':
+
+            A: array-like, shape = [n_groupings]
+                True positive rates
+
+            B: array-like, shape = [n_groupings]
+                False positive rates
+
+    mode: {'binary', 'sample', 'rate'}, optional
+        Directs the interpretation of A and B
 
     max_value: float, optional
         Maximum possible d-prime value. Default is ``np.inf``.
@@ -57,11 +75,12 @@ def dprime(y_pred=None, y_true=None, pos=None, neg=None, max_value=np.inf, min_v
     """
 
     # -- basic checks and conversion
-    if pos is not None and neg is not None:
-        pos = np.array(pos)
-        neg = np.array(neg)
+    if mode == 'sample':
+        pos, neg = np.array(A), np.array(B)
 
-    else:
+    elif mode == 'binary':
+        y_true, y_pred = A, B
+
         assert len(y_true) == len(y_pred)
         assert np.isfinite(y_true).all()
 
@@ -71,48 +90,64 @@ def dprime(y_pred=None, y_true=None, pos=None, neg=None, max_value=np.inf, min_v
         y_pred = np.array(y_pred)
         assert y_pred.ndim == 1
 
-        # -- actual computation
         i_pos = y_true > 0
         i_neg = ~i_pos
 
         pos = y_pred[i_pos]
         neg = y_pred[i_neg]
 
-    assert np.isfinite(pos).all()
-    assert np.isfinite(neg).all()
+    elif mode == 'rate':
+        TPR, FPR = np.array(A), np.array(B)
+        assert TPR.shape == FPR.shape
 
-    if pos.size <= 1:
-        raise ValueError('Not enough positive samples to estimate the variance')
-    if neg.size <= 1:
-        raise ValueError('Not enough negative samples to estimate the variance')
+    else:
+        raise ValueError('Invalid mode')
 
     # -- compute d'
-    pos_mean = pos.mean()
-    neg_mean = neg.mean()
-    pos_var = pos.var(ddof=1)
-    neg_var = neg.var(ddof=1)
+    if mode == 'sample' or mode == 'binary':
+        assert np.isfinite(pos).all()
+        assert np.isfinite(neg).all()
 
-    num = pos_mean - neg_mean
-    div = np.sqrt((pos_var + neg_var) / 2.)
+        if pos.size <= 1:
+            raise ValueError('Not enough positive samples'\
+                    'to estimate the variance')
+        if neg.size <= 1:
+            raise ValueError('Not enough negative samples'\
+                    'to estimate the variance')
+
+        pos_mean = pos.mean()
+        neg_mean = neg.mean()
+        pos_var = pos.var(ddof=1)
+        neg_var = neg.var(ddof=1)
+
+        num = pos_mean - neg_mean
+        div = np.sqrt((pos_var + neg_var) / 2.)
+
+        dp = num / div
+
+    else:   # mode == 'rate'
+        dp = norm.ppf(TPR) - norm.ppf(FPR)
 
     # from Dan's suggestion about clipping d' values...
-    dp = np.clip(num / div, min_value, max_value)
+    dp = np.clip(dp, min_value, max_value)
 
     return dp
 
 
-def dprime_from_confusion_matrix(M, max_value=np.inf, min_value=-np.inf, **kwargs):
-    """Computes the d-prime sensitivity indices of the given confusion matrix.
-    This function is designed mostly for when there is no access to internal 
-    representations and/or decision making mechanisms (like human data).  
-    If no ``collation`` is defined in ``kwargs`` this function computes 
+def dprime_from_confusion_matrix(M, max_value=np.inf, \
+        min_value=-np.inf, **kwargs):
+    """Computes the d-prime sensitivity indices of predictions from
+    the given confusion matrix. This function is designed mostly for
+    when there is no access to internal representations and/or
+    decision making mechanisms (like human data).  If no ``collation``
+    is defined in ``kwargs`` this function computes
     one vs. rest d-prime for each class.
 
     Parameters
     ----------
-    M: array-like, shape = [n_classes (true), n_classes (pred)] 
+    M: array-like, shape = [n_classes (true), n_classes (pred)]
         Confusion matrix, where the element M_{rc} means the number of
-        times when the classifier/subject guesses that a test sample in 
+        times when the classifier/subject guesses that a test sample in
         the r-th class belongs to the c-th class.
 
     max_value: float, optional
@@ -122,9 +157,10 @@ def dprime_from_confusion_matrix(M, max_value=np.inf, min_value=-np.inf, **kwarg
         Minimum possible d-prime value. Default is ``-np.inf``.
 
     kwargs: named arguments, optional
-        Passed to ``confusion_stats()``.  By passing ``collation``, ``fudge_mode``,
-        ``fudge_factor``, etc. one can change the behavior of d-prime computation 
-        (see ``confusion_stats()`` for details). 
+        Passed to ``confusion_stats()``.  By assigning ``collation``,
+        ``fudge_mode``, ``fudge_factor``, etc. one can change the
+        behavior of d-prime computation
+        (see ``confusion_stats()`` for details).
 
     Returns
     -------
@@ -143,7 +179,6 @@ def dprime_from_confusion_matrix(M, max_value=np.inf, min_value=-np.inf, **kwarg
 
     TPR = TP / P
     FPR = FP / N
-    dp = np.clip(norm.ppf(TPR) - norm.ppf(FPR), min_value, max_value)
 
-    return dp
-
+    return dprime(TPR, FPR, mode='rate', \
+            max_value=max_value, min_value=min_value)
--- bangmetric/utils.py
+++ bangmetric/utils.py
@@ -14,23 +14,23 @@ DEFAULT_FUDGE_MODE = 'correction'
 
 def confusion_matrix_stats(M, collation=None, \
         fudge_mode=DEFAULT_FUDGE_MODE, fudge_factor=DEFAULT_FUDGE_FACTOR):
-    """Computes classification statistics of sub-confusion matrices inside 
+    """Computes classification statistics of sub-confusion matrices inside
     the given original confusion matrix M.  If no ``collation`` is given,
     statistics for each one vs. rest sub-confusion matrix will be computed.
 
     Parameters
     ----------
-    M: array-like, shape = [n_classes (true), n_classes (pred)] 
+    M: array-like, shape = [n_classes (true), n_classes (pred)]
         Confusion matrix, where the element M_{rc} means the number of
         times when the classifier guesses that a test sample in the r-th class
         belongs to the c-th class.
 
-    collation: None or array-like with shape = [n_groupings, n_classes], optional
-        Defines how to group entries in `M` to make sub-confusion matrices.  
+    collation: None or array-like of shape = [n_groupings, n_classes], optional
+        Defines how to group entries in `M` to make sub-confusion matrices.
         Entries shoule be {+1, 0, -1}.  A row defines one instance of grouping,
         where +1, -1, and 0 designate the corresponding class as a
-        positive, negative, and ignored class, respectively.  For example, 
-        the following `collation` defines a 3-way one vs. rest grouping 
+        positive, negative, and ignored class, respectively.  For example,
+        the following `collation` defines a 3-way one vs. rest grouping
         (given that `M` is a 3x3 matrix):
             [[+1, -1, -1],
              [-1, +1, -1],
@@ -47,23 +47,21 @@ def confusion_matrix_stats(M, collation=None, \
             'always': always apply the fudge factor
             'none': no fudging --- equivalent to ``fudge_factor=0``
 
-
     Returns
     -------
     P: array, shape = [n_groupings]
-        Array of the number of positives, where each element corresponds to each 
-        grouping (row) defined by `collation`.
+        Array of the number of positives, where each element corresponds to
+        each grouping (row) defined by `collation`.
     N: array, shape = [n_groupings]
         Same as P, except that this is an array of the number of negatives.
     TP: array, shape = [n_groupings]
-        Same as P, except that this is an array of the number of true positives.
+        Same as P, except an array of the number of true positives.
     TN: array, shape = [n_groupings]
-        Same as P, except that this is an array of the number of true negatives.
+        Same as P, except an array of the number of true negatives.
     FP: array, shape = [n_groupings]
-        Same as P, except that this is an array of the number of false positives.
+        Same as P, except an array of the number of false positives.
     FN: array, shape = [n_groupings]
-        Same as P, except that this is an array of the number of false negatives.
-
+        Same as P, except an array of the number of false negatives.
 
     References
     ----------
@@ -77,7 +75,7 @@ def confusion_matrix_stats(M, collation=None, \
     assert M.shape[0] == M.shape[1]
     n_classes = M.shape[0]
 
-    if collation is None:    
+    if collation is None:
         # make it one vs. rest.  E.g., for a 3-classes case:
         #  [[+1, -1, -1],
         #   [-1, +1, -1],
@@ -88,19 +86,25 @@ def confusion_matrix_stats(M, collation=None, \
         collation = np.array(collation, dtype='int8')
         assert collation.ndim == 2
         assert collation.shape[1] == n_classes
-    
+
     # P0: number of positives, for each class
     # P: number of positives, for each grouping
     # N: number of negatives, for each grouping
     # TP: number of true positives, for each grouping
     # FP: number of false positives, for each grouping
-    P0 = np.sum(M, axis=1)   
-    P = np.array([np.sum(P0[coll == +1]) for coll in collation], dtype='float64')
-    N = np.array([np.sum(P0[coll == -1]) for coll in collation], dtype='float64')
-    TP = np.array([np.sum(M[coll == +1][:, coll == +1]) for coll in collation], dtype='float64')
-    TN = np.array([np.sum(M[coll == -1][:, coll == -1]) for coll in collation], dtype='float64')
-    FP = np.array([np.sum(M[coll == -1][:, coll == +1]) for coll in collation], dtype='float64')
-    FN = np.array([np.sum(M[coll == +1][:, coll == -1]) for coll in collation], dtype='float64')
+    P0 = np.sum(M, axis=1)
+    P = np.array([np.sum(P0[coll == +1]) \
+            for coll in collation], dtype='float64')
+    N = np.array([np.sum(P0[coll == -1]) \
+            for coll in collation], dtype='float64')
+    TP = np.array([np.sum(M[coll == +1][:, coll == +1]) \
+            for coll in collation], dtype='float64')
+    TN = np.array([np.sum(M[coll == -1][:, coll == -1]) \
+            for coll in collation], dtype='float64')
+    FP = np.array([np.sum(M[coll == -1][:, coll == +1]) \
+            for coll in collation], dtype='float64')
+    FN = np.array([np.sum(M[coll == +1][:, coll == -1]) \
+            for coll in collation], dtype='float64')
 
     # -- application of fudge factor
     if fudge_mode == 'none':           # no fudging
@@ -111,8 +115,8 @@ def confusion_matrix_stats(M, collation=None, \
         FP += fudge_factor
         TN += fudge_factor
         FN += fudge_factor
-        P += 2.*fudge_factor
-        N += 2.*fudge_factor
+        P += 2. * fudge_factor
+        N += 2. * fudge_factor
 
     elif fudge_mode == 'correction':   # apply fudge factor only when needed
         TP[TP == P] = P[TP == P] - fudge_factor    # 100% correct
@@ -120,14 +124,13 @@ def confusion_matrix_stats(M, collation=None, \
         FP[FP == N] = N[FP == N] - fudge_factor    # always FAR
         FP[FP == 0] = fudge_factor                 # no false alarm
 
-        TN[TN == N] = N[TN == N] - fudge_factor    
-        TN[TN == 0] = fudge_factor                 
-        FN[FN == P] = P[FN == P] - fudge_factor    
-        FN[FN == 0] = fudge_factor                 
+        TN[TN == N] = N[TN == N] - fudge_factor
+        TN[TN == 0] = fudge_factor
+        FN[FN == P] = P[FN == P] - fudge_factor
+        FN[FN == 0] = fudge_factor
 
     else:
         raise ValueError('Invalid fudge_mode')
 
     # -- done
     return P, N, TP, TN, FP, FN
-
